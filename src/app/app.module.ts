2ceda09be93f5c400de41e3fa21e0429ad1f3999
Thang Phuoc Nguyen <thangphuocnguyen@gmail.com>
2019-07-18 18:44:38 +0700

    Add a service worker to project:
    `ng add @angular/pwa --project AngularPWA`
    
    included of:
    
    + Add the @angular/service-worker package to project and enable the service worker build support to angular CLI.
    
    + Import and register the service worker in application module app.module.ts file
    
    + Update the index.html file and include the manifest.json file and adds the appropriate <meta> tags for the theme-color
    
    + Install sample icon files.
    
    + Create a service worker configuration called ngsw-config.json which specifies the caching behavior.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './home/home.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
