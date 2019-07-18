0c6b3a6d674af39c8652a4a6d5d4f8babff51abd
Thang Phuoc Nguyen <thangphuocnguyen@gmail.com>
2019-07-18 17:40:16 +0700

    Add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectorRef,
+  EventEmitter,
+  Output } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPWA';
+  mobileQuery: MediaQueryList;
+  nav = [
+    {
+      'title': 'Home',
+      'path': '/'
+    },
+    {
+      'title': 'My Account',
+      'path': '/auth'
+    }
+  ];
+  private _mobileQueryListener: () => void;
+  @Output() toggleSideNav = new EventEmitter();
+
+  constructor( changeDetectorRef: ChangeDetectorRef, media: MediaMatcher ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  toggleMobileNav(nav: MatSidenav) {
+    if (this.mobileQuery.matches) {
+      nav.toggle();
+    }
+  }
 }
